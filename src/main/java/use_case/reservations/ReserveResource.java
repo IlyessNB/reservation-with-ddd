29f8909570230212e21868dc0f1339146264dd5d
louis <Louiis1997@hotmail.fr>
2023-04-13 18:11:06 +0200

    refacto : inject interface and not class

--- src/main/java/use_case/reservations/ReserveResource.java
+++ src/main/java/use_case/reservations/ReserveResource.java
@@ -1,11 +1,11 @@
 package use_case.reservations;
 
-import infrastructure.repository.InMemoryReservationRepository;
-import infrastructure.repository.InMemoryResourceRepository;
-import infrastructure.repository.InMemoryUserRepository;
 import model.institution.resource.Resource;
 import model.institution.resource.ResourceIsClosedException;
 import model.institution.resource.ResourceNotFoundException;
+import model.repositories.ReservationRepository;
+import model.repositories.ResourceRepository;
+import model.repositories.UserRepository;
 import model.reservation.ConflictualReservationsException;
 import model.reservation.Reservation;
 
@@ -14,17 +14,15 @@ import java.time.LocalTime;
 import java.util.List;
 
 public class ReserveResource {
+    UserRepository userRepository;
+    ReservationRepository reservationRepository;
+    ResourceRepository resourceRepository;
 
-    InMemoryUserRepository inMemoryUserRepository;
-    InMemoryReservationRepository inMemoryReservationRepository;
-    InMemoryResourceRepository inMemoryResourceRepository;
-
-    public ReserveResource(InMemoryUserRepository inMemoryUserRepository, InMemoryReservationRepository inMemoryReservationRepository, InMemoryResourceRepository inMemoryResourceRepository) {
-        this.inMemoryUserRepository = inMemoryUserRepository;
-        this.inMemoryReservationRepository = inMemoryReservationRepository;
-        this.inMemoryResourceRepository = inMemoryResourceRepository;
+    public ReserveResource(UserRepository userRepository, ReservationRepository reservationRepository, ResourceRepository resourceRepository) {
+        this.userRepository = userRepository;
+        this.reservationRepository = reservationRepository;
+        this.resourceRepository = resourceRepository;
     }
-
 
     public Reservation reserve(String userId, String resourceId, LocalTime startTime, LocalTime endTime, LocalDate date) throws ResourceNotFoundException, ResourceIsClosedException, ConflictualReservationsException {
         Reservation reservation = Reservation.create(userId, resourceId, startTime, endTime, date);
@@ -32,13 +30,13 @@ public class ReserveResource {
             throw new IllegalArgumentException("La reservation n'est pas valide");
         }
 
-        Resource resource = inMemoryResourceRepository.findById(resourceId);
-        List<Reservation> reservationRepositoryByDate = inMemoryReservationRepository.findByDate(resourceId, date);
+        Resource resource = resourceRepository.findById(resourceId);
+        List<Reservation> reservationRepositoryByDate = reservationRepository.findByDate(resourceId, date);
 
         resource.isOpen(startTime, endTime, date);
         reservation.checkConflicts(startTime, endTime, date, reservationRepositoryByDate);
 
-        inMemoryReservationRepository.add(reservation);
+        reservationRepository.add(reservation);
         return reservation;
     }
 }
